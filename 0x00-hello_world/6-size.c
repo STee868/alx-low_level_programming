fdbf1333783f86986cc042a460b07cf069726d9e
“Steve <stevethiga@live.coml>
2023-03-17 15:09:47 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
- * main - a C program that prints the size of various types on the computer it is compiled and run on
+ * main - a program that prints size of various types on the computer it is compiled & run on
  * Return: 0 (Success)
  */
 
-int main() {
-    char a;
-    int b;
-    long int c;
-    long long int d;
-    float f;
+int main(void) 
+{   
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
     printf("Size of a char: %lu byte(s)\n", sizeof(a));
     printf("Size of an int: %lu byte(s)\n", sizeof(b));
@@ -17,5 +18,5 @@ int main() {
     printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
     printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
-    return 0;
+    return (0);
 }
