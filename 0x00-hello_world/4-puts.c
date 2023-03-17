fd7e92a4b341453a6d4fba5a2322af1844c5f778
“Steve <stevethiga@live.coml>
2023-03-17 08:34:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /** 
- * A C program that prints a line using put function
- * Return: 0 (success)
+ *A C program that prints a line using put function
+ *Return: 0 (success)
  **/
-
 int main(void) 
 {
  puts("\"Programming is like building a multilingual puzzle");
