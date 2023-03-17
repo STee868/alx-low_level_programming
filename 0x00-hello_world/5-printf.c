0d50148251ee3d3c3a6af46aa0f27d753aa93533
“Steve <stevethiga@live.coml>
2023-03-17 09:01:25 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**main - A C program that prints a line using printf
+ *Return: 0 (Success)
+ **/
+int main(void){
+    printf("Behold, a masterpiece of the finest caliber,\n"
+           "crafted with skill and attention to detail.\n"
+           "Each stroke of the brush imbued with passion and purpose,\n"
+           "resulting in a work of art that is truly exceptional.\n");
+    return 0;
+}
