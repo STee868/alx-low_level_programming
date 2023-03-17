0db9f856b27ae218c3c0eba2da80f16b73ff00eb
“Steve <stevethiga@live.coml>
2023-03-17 09:18:26 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,6 @@
  *Return: 0 (Success)
  **/
 int main(void){
-    printf("Behold, a masterpiece of the finest caliber,\n"
-           "crafted with skill and attention to detail.\n"
-           "Each stroke of the brush imbued with passion and purpose,\n"
-           "resulting in a work of art that is truly exceptional.\n");
-    return 0;
+	printf("with proper grammar, but the outcome is a piece of art\,n")
+	return 0;
 }
