a18d8de90943f4fa1b566596a74a1834e65d5114
“Steve <stevethiga@live.coml>
2023-03-17 14:35:02 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,5 +15,6 @@ int main(void)
         printf("Size of long int: %lu byte(s)\n", sizeof(c));
         printf("Size of long long int: %lu byte(s)\n", sizeof(d));
         printf("Size of float: %lu byte(s)\n", sizeof(f));
-return 0;
+	
+	return (0);
 }
