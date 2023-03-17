e1997d8d0d824c5b869f66215be5b2f22e6dfbdd
“Steve <stevethiga@live.coml>
2023-03-17 09:29:17 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**main - A C program that prints a line using printf
  *Return: 0 (Success)
+ **/
+int main (void);
 	printf("with proper grammar, but the outcome is a piece of art\,n");
 	return (0);
 }
