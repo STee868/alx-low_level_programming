0fb8c3f3bc116fb2b8663cfb64caafa2e93accab
“Steve <stevethiga@live.coml>
2023-03-19 15:06:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 {
 	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(STDERR_FILENO, msg, sizeof(char) * 59);
+	fputs(msg, stderr);
 	return (1);
 }
