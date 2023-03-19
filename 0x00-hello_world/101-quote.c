bd4ad882553838063693e05be9d579878a14509a
“Steve <stevethiga@live.coml>
2023-03-19 14:44:27 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - A program that prints exact line to the standard error
  * Return 1 (success)
 */
 int main(void)
 {
-	char *msg = "and that piece of art is useful\"- Dora Korpar,2015-10-19";
+	char *msg = "and that piece of art is useful\" - Dora Korpar,2015-10-19";
 
 	write(STDERR_FILENO, msg, sizeof(char) * 59);
 	return (1);
