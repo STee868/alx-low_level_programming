14f294e69979def48815ae4c594a48ae64584ffe
“Steve <stevethiga@live.coml>
2023-03-19 13:57:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,6 @@
 */
 int main(void)
 {
-	const char *msg = "and that piece of art is useful\"- Dora Korpar,2015-10-19";
-
-	write(STDERR_FILENO, msg, 59);
-	return(1);
-
+	fput("and that piece of art is useful\"- Dora Korpar,2015-10-19"\n, stdout);
+	return (1);
 }
-
