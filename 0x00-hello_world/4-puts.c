597432adcece30100acece9882c7531dead426cf
“Steve <stevethiga@live.coml>
2023-03-19 12:21:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  *A C program that prints a line using put function
  *Return: 0 (success)
  **/
-int main(void) 
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
