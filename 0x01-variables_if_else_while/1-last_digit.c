6b849b2fa92ecc0e4d6da6d2c0a7afa10b65d2d7
“Steve <stevethiga@live.coml>
2023-03-20 06:51:38 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+using namespace std;
+/**
+*main- program assigns a random number to the variable n
+*result: 0 (success)
+**/
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	cout << "Last digit of " << n << " is ";
+	if (n % 10 > 5)
+	{
+		cout << n % 10 << " and is greater than 5" << endl;
+	else if (n % 10 == 0)
+		cout << n % 10 << " and is 0" << endl;
+	else
+		cout << n % 10 << " and is less than 6 and not 0" << endl;
+	}
+return (0);
+}
