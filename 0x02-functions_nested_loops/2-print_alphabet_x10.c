00a4d51f9d47ea524c896eba2bddcfb901e853a7
“Steve <stevethiga@live.coml>
2023-03-21 15:07:08 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
  * print_alphabet_x10 - function that prints 10 times the alphabet in lowercase
  * Return: 0 (success)
  **/
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 	char letter;
 	int i;
