92580aefde11ce24a69ab6fa34beaa1fc7e90210
“Steve <stevethiga@live.coml>
2023-03-21 15:31:33 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
+
 /**
- * _islower - checks for lowercase character
+ *test_islower - checks for lowercase character
  * @c: character to check
  *
  * Return: 1 if c is lowercase, 0 otherwise
  */
-int _islower(int c)
+int test_islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
