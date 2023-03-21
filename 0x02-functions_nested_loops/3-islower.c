a54a49cea0f167456827ef5c04711033a9940a02
“Steve <stevethiga@live.coml>
2023-03-21 16:08:52 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,37 +8,10 @@
  */
 int _putchar(char c);
 int _islower(int c);
-void test_islower(char n);
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * _islower - Checks if a character is lowercase
- * @c: The character to check
- *
- * Return: 1 if c is lowercase, 0 otherwise
- */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
-}
-
-/**
- * test_islower - Tests the _islower function
- * @n: The character to check
- */
-void test_islower(char n)
-{
-	int r;
-
-	r = _islower(n);
-	if (r == 1)
-		_putchar('1');
-	else
-		_putchar('0');
 }
