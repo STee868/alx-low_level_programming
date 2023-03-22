e98512d326315f3bc776b661d97e535319a785ef
“Steve <stevethiga@live.coml>
2023-03-22 01:39:34 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,23 @@
 #include "main.h"
+void test_isalpha(int n)
+{
+    int r;
+
+    r = _isalpha(n);
+    _putchar(r + '0');
+    _putchar('\n');
+}
+
+int main(void)
+{
+    test_isalpha('H');
+    test_isalpha('o');
+    test_isalpha(108);
+    test_isalpha(';');
+    test_isalpha('\n');
+
+    return (0);
+}
 /**
  * _isalpha - checks for alphabetic character
  * @c: the character to check
