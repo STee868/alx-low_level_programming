43d6a16a7f8536f9d622ecf27e42578fbd9bcbcb
“Steve <stevethiga@live.coml>
2023-03-22 01:50:57 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,49 @@
-#include "main.h"
-void test_isalpha(int n)
-{
-    int r;
+#include <unistd.h>
 
-    r = _isalpha(n);
-    _putchar(r + '0');
-    _putchar('\n');
+/**
+ * _putchar - writes a character to standard output
+ * @c: the character to write
+ *
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
 
+/**
+ * test_isalpha - tests the _isalpha function for a given input
+ * @n: the input to test
+ *
+ * Returns: none
+ */
+void test_isalpha(int n)
+{
+	int r;
+	r = _isalpha(n);
+	_putchar(r + '0');
+	_putchar('\n');
+}
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to check
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * main - entry point for the program
+ *
+ * Return: always 0
+ */
 int main(void)
 {
     test_isalpha('H');
@@ -17,21 +53,4 @@ int main(void)
     test_isalpha('\n');
 
     return (0);
-}
-/**
- * _isalpha - checks for alphabetic character
- * @c: the character to check
- *
- * Return: 1 if c is a letter, lowercase or uppercase
- *         0 otherwise
- */
-int _isalpha(int c);
-int_putchar(char c);
-
-int _isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
 }
