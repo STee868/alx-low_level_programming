3eca75c8ad2d031f19cb32aab7729d209c6d844e
“Steve <stevethiga@live.coml>
2023-03-22 01:05:23 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,32 +1,15 @@
-#include <stdio.h>
 #include <ctype.h>
 
 /**
- * _putchar - Writes the character c to stdout
- * @c: The character to print
+ * _islower - function checks for lower case character
  *
- * Return: On success 1, on error -1
+ * Return:0 (success)
  */
-int _putchar(char c);
-test_islower(int n);
-int main(void);
+void _islower(void);
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-                return (1);
-        else
-                return (0);
-}
-void test_islower(int n)
-{
-        int r;
-
-        r = _islower(n);
-        _putchar(r + '0');
-        _putchar('\n');
-}
-int main(void)
-{
-        test_islower('h');
-        return (0);
+	if (islower(c))
+		return (1);
+	else
+		return (0);
 }
