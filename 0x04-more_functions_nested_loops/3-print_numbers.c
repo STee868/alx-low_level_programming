9a71f845360065fb1349739dd950fdb037c36753
“Steve <stevethiga@live.coml>
2023-03-23 08:42:41 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,11 +11,8 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-		_putchar(i + '0')
-			_putchar('\n');
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
 }
-int main(void);
-{
-	return (0);
-}
-
