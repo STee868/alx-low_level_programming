6cc3c93fb96398d3e0ffac225f838799c7ff1bda
“Steve <stevethiga@live.coml>
2023-03-30 14:02:05 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * main - a function that concatenates two strings
+ * @dest: The destination string to append to.
+ * @src: The source string to append from.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+
+int main(void)
+{
+	char str1[100] = "Hello, ";
+	char str2[] = "world!";
+
+	_strcat(str1, str2);
+	_putchar_str(str1);
+	return (0);
+}
+void _putchar(char c)
+{
+	write(1, &c, 1);
+}
+void _putchar_str(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
+char *_strcat(char *dest, char *src)
+{
+	char *dest_ptr = dest;
+
+	while (*dest_ptr != '\0')
+	{
+		dest_ptr++;
+	}
+	while (*src != '\0')
+	{
+		*dest_ptr = *src;
+		dest_ptr++;
+		src++;
+	}
+	*dest_ptr = '\0';
+
+	return (dest);
+}
+
