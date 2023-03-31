d2bcf98315c4f14c252747baf37122f58504123e
“Steve <stevethiga@live.coml>
2023-03-31 16:02:59 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,51 +1,74 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * main - a function that concatenates two strings
- * @dest: The destination string to append to.
- * @src: The source string to append from.
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
  *
  * Return: A pointer to the resulting string dest.
  */
-
-int main(void)
-{
-	char str1[100] = "Hello, ";
-	char str2[] = "world!";
-
-	_strcat(str1, str2);
-	_putchar_str(str1);
-	return (0);
-}
-int _putchar(char c)
-{
-	write(1, &c, 1);
-}
-int _putchar_str(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-}
 char *_strcat(char *dest, char *src)
 {
-	char *dest_ptr = dest;
+    int dest_len = 0, i;
 
-	while (*dest_ptr != '\0')
-	{
-		dest_ptr++;
-	}
-	while (*src != '\0')
-	{
-		*dest_ptr = *src;
-		dest_ptr++;
-		src++;
-	}
-	*dest_ptr = '\0';
+    while (dest[dest_len] != '\0')
+    {
+        dest_len++;
+    }
 
-	return (dest);
+    for (i = 0; src[i] != '\0'; i++)
+    {
+        dest[dest_len + i] = src[i];
+    }
+
+    dest[dest_len + i] = '\0';
+
+    return (dest);
+}
+
+/**
+ * _putchar - Writes a character to the standard output.
+ * @c: The character to be written.
+ *
+ * Return: 0 on success, -1 on error.
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+
+/**
+ * _putchar_str - Writes a string to the standard output.
+ * @str: The string to be written.
+ *
+ * Return: void.
+ */
+void _putchar_str(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i++;
+    }
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    ptr = _strcat(s1, s2);
+
+    _putchar_str(ptr);
+
+    return (0);
 }
 
