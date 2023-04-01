8397821430bfca404af8dcb5feb7bc0eefd7fa74
“Steve <stevethiga@live.coml>
2023-04-01 00:53:22 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,27 @@
-**
+#include "main.h"
+/**
  * _strncpy - Copies a string, up to n characters.
  * @dest: The destination buffer to copy to.
  * @src: The source string to copy from.
  * @n: The maximum number of characters to copy.
  *
- * Return: A pointer to the destination buffer dest.
+ * Return: A pointer to the destination string.
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, const char *src, size_t n)
 {
-    int i;
-
-    // Copy at most n characters from the source string to the destination buffer
-    for (i = 0; i < n && src[i] != '\0'; i++) {
+    /* Copy at most n characters from the source string to the destination buffer */
+    size_t i;
+    for (i = 0; i < n && src[i] != '\0'; i++)
+    {
         dest[i] = src[i];
     }
 
-    // If n is greater than the length of src, fill the remaining bytes with null characters
-    for ( ; i < n; i++) {
+    /* If the source string is shorter than n, fill the remaining characters with null bytes */
+    for (; i < n; i++)
+    {
         dest[i] = '\0';
     }
 
-    // Return a pointer to the destination buffer
+    /* Return a pointer to the destination string */
     return dest;
 }
