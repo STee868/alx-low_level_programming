f16fffab6f07f5720ffc0048e0ee595e41c1cfe6
“Steve <stevethiga@live.coml>
2023-04-10 10:32:17 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,17 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Prints the name of the program
+ * main - prints the name of the program
  *
- * @argc: Number of arguments passed to the program
- * @argv: Array of pointers to the strings which are those arguments
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of pointers to the arguments
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+    (void) argc;
+
     /* Print the first argument, which is the name of the program */
     printf("%s\n", argv[0]);
 
