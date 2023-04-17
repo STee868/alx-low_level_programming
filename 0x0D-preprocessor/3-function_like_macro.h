e509225475cac08b08601e72f8adc26f5e146499
“Steve <stevethiga@live.coml>
2023-04-17 11:32:49 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,25 +1,15 @@
 #include <stdio.h>
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#include "abs.h"
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
-#define SUM(x, y) ((x) + (y))
-#define PRODUCT(x, y) ((x) * (y))
-
-#endif /* FUNCTION_LIKE_MACRO_H */
-
 
 int main(void)
 {
 	int a = -5;
-	int b = 10;
-	double c = -3.14;
-	double d = 2.71;
+	int abs_a = ABS(a);
 
-	printf("ABS(%d) = %d\n", a, ABS(a));
-	printf("ABS(%d) = %d\n", b, ABS(b));
-	printf("ABS(%.2f) = %.2f\n", c, ABS(c));
-	printf("ABS(%.2f) = %.2f\n", d, ABS(d));
+	printf("The absolute value of %d is %d\n", a, abs_a);
 
 	return (0);
 }
+
