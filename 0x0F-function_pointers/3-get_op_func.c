b112e3ce0135b612e7443d3947baf59aa8f6acf6
“Steve <stevethiga@live.coml>
2023-04-19 15:33:27 -0700

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,29 +2,27 @@
 
 /**
  * get_op_func - selects the correct function to perform the operation
- * @s: the operator passed as argument
+ * @s: operator passed as argument
  *
- * Return: a pointer to the corresponding function
+ * Return: pointer to corresponding function
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+	int i = 0;
 
-    while (ops[i].op != NULL)
-    {
-        if (*(ops[i].op) == *s)
-            return (ops[i].f);
-        i++;
-    }
-
-    return (NULL);
+	while (ops[i].op)
+	{
+		if (*ops[i].op == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
-
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,37 +1,43 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
 
 /**
- * main - Entry point
- * @argc: The number of command-line arguments
- * @argv: An array of pointers to the arguments
+ * main - performs simple operations on two integers
+ * @argc: the number of command-line arguments
+ * @argv: an array of strings containing the command-line arguments
  *
- * Return: Always 0
+ * Return: 0 if successful, otherwise a status code
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    int a, b, result;
-    int (*op_func)(int, int);
+	int num1, num2, result;
+	int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
-    op_func = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    if (op_func == NULL || argv[2][1] != '\0')
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		return (100);
+	}
+	op_func = get_op_func(argv[2]);
 
-    result = op_func(a, b);
-    printf("%d\n", result);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	result = op_func(num1, num2);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
 
