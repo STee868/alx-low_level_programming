4582c70b7d1bb39a519c5b76332e874f10637468
“Steve <stevethiga@live.coml>
2023-04-28 11:35:29 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
  * print_list - prints all the elements of a list_t list
- * @h: pointer to the list
+ * @h: pointer to the list_t list
  *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
+	const list_t *current = h;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
+	while (current != NULL)
+	{
+		if (current->str != NULL)
+			printf("[%d] %s\n", current->len, current->str);
+		else
+			printf("[0] (nil)\n");
 
-        count++;
-        h = h->next;
-    }
+		count++;
+		current = current->next;
+	}
 
-    return (count);
+	return (count);
 }
-
--- 0x12-singly_linked_lists/main.c
+++ 0x12-singly_linked_lists/main.c
@@ -1,53 +1,34 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
 int main(void)
 {
-    /* Test list_len function */
     list_t *head = NULL;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
+    list_t *new_node;
+    list_t *end_node;
     size_t n;
 
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error: unable to allocate memory\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = list_len(head);
-    printf("Number of elements in the list: %lu\n", n);
+    printf("Number of nodes: %lu\n", list_len(head));
 
-    /* Test add_node function */
-    list_t *new_node;
-    new_node = add_node(&head, "New Node");
+    new_node = add_node(&head, "Holberton");
     if (new_node == NULL)
     {
-        printf("Error: unable to add new node\n");
+        printf("Failed to add new node\n");
         return (1);
     }
-    printf("Added new node with value '%s'\n", new_node->str);
 
-    /* Test add_node_end function */
-    list_t *end_node;
-    end_node = add_node_end(&head, "End Node");
+    end_node = add_node_end(&head, "School");
     if (end_node == NULL)
     {
-        printf("Error: unable to add new node at end\n");
+        printf("Failed to add end node\n");
         return (1);
     }
-    printf("Added new node at end with value '%s'\n", end_node->str);
 
-    /* Test free_list function */
+    n = list_len(head);
+    printf("Number of nodes: %lu\n", n);
+
     free_list(head);
-    printf("Freed memory\n");
 
     return (0);
 }
