64abdf8ab16db0dfdefedc5dc357cffff6018d88
“Steve <stevethiga@live.coml>
2023-04-28 11:09:20 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,27 +11,24 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    unsigned int len;
+	list_t *new_node;
+	unsigned int len;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    for (len = 0; str[len]; len++)
-        ;
+	for (len = 0; str[len]; len++)
+		;
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-
-    new_node->len = len;
-    new_node->next = *head;
-    *head = new_node;
-
-    return (new_node);
+	return (new_node);
 }
-
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,37 +10,35 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *last_node;
-    size_t str_len = 0;
+	list_t *new_node, *last_node;
+	size_t str_len = 0;
 
-    /* Get the length of the string */
-    while (str && str[str_len])
-        str_len++;
+	/* Get the length of the string */
+	while (str && str[str_len])
+		str_len++;
 
-    /* Create the new node */
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Create the new node */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    new_node->len = str_len;
-    new_node->next = NULL;
+	new_node->str = strdup(str);
+	new_node->len = str_len;
+	new_node->next = NULL;
 
-    /* If list is empty, make new node the head */
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+	/* If list is empty, make new node the head */
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    /* Find the last node in the list */
-    last_node = *head;
-    while (last_node->next != NULL)
-        last_node = last_node->next;
+	/* Find the last node in the list */
+	last_node = *head;
+	while (last_node->next != NULL)
+		last_node = last_node->next;
+	/* Add the new node to the end */
+	last_node->next = new_node;
 
-    /* Add the new node to the end */
-    last_node->next = new_node;
-
-    return (new_node);
+	return (new_node);
 }
-
