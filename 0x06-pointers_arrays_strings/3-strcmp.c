93d261d3ac9438a2cb3fe062be7f799a8b02e531
“Steve <stevethiga@live.coml>
2023-04-30 09:23:50 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-    int i;
+	int i;
 
-    for (i = 0; s1[i] && s2[i]; i++)
-    {
-        if (s1[i] != s2[i])
-            return (s1[i] - s2[i]);
-    }
+	for (i = 0; s1[i] && s2[i]; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
 
-    if (!s1[i] && !s2[i])
-        return (0);
-    else if (!s1[i])
-        return (-s2[i]);
-    else
-        return (s1[i]);
+	if (!s1[i] && !s2[i])
+		return (0);
+	else if (!s1[i])
+		return (-s2[i]);
+	else
+		return (s1[i]);
 }
