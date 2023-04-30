096a9c2032a50879ae1a280c3f57d8d7254f4de7
“Steve <stevethiga@live.coml>
2023-04-30 09:07:25 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,58 +1,27 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _strcat - Concatenates two strings
+ * @dest: Pointer to destination string
+ * @src: Pointer to source string
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Pointer to concatenated string
  */
-int _putchar(char c)
+char *_strcat(char *dest, char *src)
 {
-    return (write(1, &c, 1));
-}
+	char *temp = dest;
 
-/**
- * _putchar_str - writes a string to stdout
- * @str: The string to print
- *
- * Return: The number of characters printed
- */
-int _putchar_str(char *str)
-{
-    int i;
+	while (*temp)
+		temp++;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        _putchar(str[i]);
-    }
+	while (*src)
+	{
+		*temp = *src;
+		temp++;
+		src++;
+	}
 
-    return (i);
-}
+	*temp = '\0';
 
-/**
- * _strcat - concatenates two strings
- * @dest: The destination string
- * @src: The source string
- *
- * Return: A pointer to the resulting string dest
- */
-char *_strcat(char *dest, const char *src)
-{
-    int dest_len = 0, i;
-
-    while (dest[dest_len] != '\0')
-    {
-        dest_len++;
-    }
-
-    for (i = 0; src[i] != '\0'; i++)
-    {
-        dest[dest_len + i] = src[i];
-    }
-
-    dest[dest_len + i] = '\0';
-
-    return (dest);
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,13 +1,13 @@
 #ifndef MAIN_H
-#define MAIN_
+#define MAIN_H
 
-int _putchar_str(char *str);
-char *_strcat(char *dest, const char *src);
-int _putchar(char c);
-char *cap_string(char *);
-char *string_toupper(char *);
-void reverse_array(int *a, int n);
-int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
 
 #endif /* MAIN_H */
+
