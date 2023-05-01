2be834f9b455e08d472c123cd0ce051016c5b3d0
“Steve <stevethiga@live.coml>
2023-05-01 04:54:38 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,8 +32,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-size_t print_listint_safe(const listint_t *head);
-void free_listint_safe(listint_t **head);
-listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif /* LISTS_H */
